d1329fb083c20aca58dca3429302c1a2d5e919cb
Ankan Basu - Enterprise Security Architect <basu.ankan@gmail.com>
2021-06-12 14:50:02 -0400

    Create webapppoweshell.ps1

---
+++ webapppoweshell.ps1
@@ -0,0 +1,7 @@
+$ResourceGroupName = "demo-ps"
+$webappname= "demo-ps-webapp-01 
+"$location= "East US" 
+
+New-AzRmResourceGroup -Name $ResourceGroupName -Location $location 
+New-AzAppServicePlan -Name $webappname -Location $location -ResourceGroupName $ResourceGroupName -Tier Free
+New-AzWebApp -Name $webappname -Location $location -AppServicePlan $webappname -ResourceGroupName $ResourceGroupName
