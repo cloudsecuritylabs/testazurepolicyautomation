2dfca18fa31841ca6f408aa8541667df63898d7a
Ankan Basu - Enterprise Security Architect <basu.ankan@gmail.com>
2021-06-12 14:59:28 -0400

    Update webapppoweshell.ps1

--- webapppoweshell.ps1
+++ webapppoweshell.ps1
@@ -2,6 +2,9 @@ $ResourceGroupName = "demo-ps"
 $webappname= "demo-ps-webapp-01" 
 $location= "East US" 
 
-New-AzRmResourceGroup -Name $ResourceGroupName -Location $location 
+New-AzResourceGroup -Name $ResourceGroupName -Location $location 
 New-AzAppServicePlan -Name $webappname -Location $location -ResourceGroupName $ResourceGroupName -Tier Free
 New-AzWebApp -Name $webappname -Location $location -AppServicePlan $webappname -ResourceGroupName $ResourceGroupName
+
+#Detete stuff when done
+Remove-AzureRmResourceGroup -Name $ResourceGroupName -Force
