99d9b77e9e178a7a0b8c59a944ae935f1a6bd2d4
Ankan Basu - Enterprise Security Architect <basu.ankan@gmail.com>
2021-06-12 15:33:19 -0400

    Create Azure_PowerShell_Deploy_WebApps.ps1

---
+++ Azure_PowerShell_Deploy_WebApps.ps1
@@ -0,0 +1,23 @@
+# Use the power of loop to create multiple web apps using Do-While Loop
+
+$ResourceGroupName = "demo-ps-multiple"
+$webappname="demo-ps-webapp"
+$location="East US"
+$NumberOfWebApps= 3
+
+# Deploy web apps using powershell
+New-AzResourceGroup -Name $ResourceGroupName -Location $location
+New-AzAppServicePlan -Name $webappname -Location $location -ResourceGroupName $ResourceGroupName -Tier Standard
+
+# Create a loop
+$i=1
+Do
+{
+  New-AzureRmWebApp -Name $webappname'-0'$i -Location $location -AppServicePlan $webappname -ResourceGroupName $ResourceGroupName
+} 
+While 
+  (($i=$i+1) -le $NumberOfWebApps)
+  
+  
+# delete resource at the end
+Remove-AzureRmResourceGroup -Name $ResourceGroupName -Force
