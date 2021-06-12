428e295651cd4dc059fe0b3d0b3c3394bdbdf29a
Ankan Basu - Enterprise Security Architect <basu.ankan@gmail.com>
2021-06-12 15:07:51 -0400

    Update AzureCLI_Deploy_WebApp.sh

--- AzureCLI_Deploy_WebApp.sh
+++ AzureCLI_Deploy_WebApp.sh
@@ -1,6 +1,8 @@
-$webappname='demo-cli-webapp-01'
-$ResourceGroupName ='demo-cli'
+# Create an sample web app using Azure CLI
+webappname='demo-cli-webapp-01'
+ResourceGroupName='demo-cli'
 
+# Deploy sample web app
 az group create --location eastus --name $ResourceGroupName
-az appservice plan create --name $webappname --resource-group $ResourceGroupName --sku Freea
+az appservice plan create --name $webappname --resource-group $ResourceGroupName --sku Free
 az webapp create --name $webappname --resource-group $ResourceGroupName --plan $webappname
