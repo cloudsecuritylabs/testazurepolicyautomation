608a5f14e7a0b3f3fcf6f6d517c2fa8a72865786
Ankan Basu - Enterprise Security Architect <basu.ankan@gmail.com>
2021-06-12 14:53:05 -0400

    Update webapppoweshell.ps1

--- webapppoweshell.ps1
+++ webapppoweshell.ps1
@@ -1,6 +1,6 @@
 $ResourceGroupName = "demo-ps"
-$webappname= "demo-ps-webapp-01 
-"$location= "East US" 
+$webappname= "demo-ps-webapp-01" 
+$location= "East US" 
 
 New-AzRmResourceGroup -Name $ResourceGroupName -Location $location 
 New-AzAppServicePlan -Name $webappname -Location $location -ResourceGroupName $ResourceGroupName -Tier Free
